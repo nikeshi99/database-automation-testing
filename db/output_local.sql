6e3b84ae67f62c5b83d4c4ccbdcad140bcad019d
Nikeshi Kumarasinghe <nikeshikumarasinghe@gmail.com>
2022-06-08 10:34:55 +0530

    liquibase docker'

--- db/output_local.sql
+++
@@ -1,5 +0,0 @@
-Liquibase History for jdbc:mysql://35.244.39.57:3306/DBdev
-
-- Database updated at 6/1/22, 8:52 AM. Applied 1 changeset(s), DeploymentId: 4073575857
-  changelog/changelog1.mysql.sql::1::nikeshi
-
