483533cbdb28a7964bcbae985bb778e18b058d4d
Nikeshi Kumarasinghe <nikeshikumarasinghe@gmail.com>
2022-06-10 12:22:15 +0530

    change yaml

--- Liquibase/db/changelog/changelog2.mysql.sql
+++ Liquibase/db/changelog/changelog2.mysql.sql
@@ -1,6 +1,6 @@
 -- liquibase formatted sql
 -- changeset nikeshi:2
-CREATE TABLE DBdev.users (
+CREATE TABLE users (
 	employee_id INTEGER AUTO_INCREMENT PRIMARY KEY,
 	first_name  VARCHAR(255) NOT NULL,
 	last_name   VARCHAR(255) NOT NULL,
