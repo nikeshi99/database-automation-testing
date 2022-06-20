05d1a9a07c124d0b965f8b0246a70fcf585a41db
Nikeshi Kumarasinghe <nikeshikumarasinghe@gmail.com>
2022-06-20 12:26:05 +0530

    change files

--- Liquibase/db/changelog/changelog-v.1.0.mysql.sql
+++ Liquibase/db/changelog/changelog-v.1.0.mysql.sql
@@ -1,12 +1,11 @@
 -- liquibase formatted sql
 -- changeset nikeshi:1 labels:v.1.0
-CREATE TABLE DBdev.Employees (
+CREATE TABLE DBdev.clients (
 	employee_id INTEGER AUTO_INCREMENT PRIMARY KEY,
 	first_name  VARCHAR(255) NOT NULL,
 	last_name   VARCHAR(255) NOT NULL,
 	email       VARCHAR(255) NOT NULL,
 	phone       VARCHAR(50) NOT NULL ,
 	hire_date   DATE NOT NULL,
-	manager_id  INTEGER REFERENCES Employees(employee_id),
 	job_title   VARCHAR(255) NOT NULL
 );
