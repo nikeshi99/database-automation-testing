683e5a1db2f280cd1a53ebcb5c8ee7be5e7d5837
Nikeshi Kumarasinghe <nikeshikumarasinghe@gmail.com>
2022-06-20 11:36:50 +0530

    change name

---
+++ Liquibase/db/changelog/changelog-v.2.0.mysql.sql
@@ -0,0 +1,10 @@
+-- liquibase formatted sql
+-- changeset nikeshi:2
+CREATE TABLE users (
+	employee_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+	first_name  VARCHAR(255) NOT NULL,
+	last_name   VARCHAR(255) NOT NULL,
+	email       VARCHAR(255) NOT NULL,
+	phone       VARCHAR(50) NOT NULL ,
+	job_title   VARCHAR(255) NOT NULL
+);
