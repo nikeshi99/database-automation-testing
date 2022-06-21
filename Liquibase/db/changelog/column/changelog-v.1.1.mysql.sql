3567a87bbccd74a6606b22c5c0b5ef87efed5ac7
Nikeshi Kumarasinghe <nikeshikumarasinghe@gmail.com>
2022-06-21 15:51:37 +0530

    deploy change files

--- Liquibase/db/changelog/column/changelog-v.1.1.mysql.sql
+++ Liquibase/db/changelog/column/changelog-v.1.1.mysql.sql
@@ -2,7 +2,9 @@
 -- changeset nikeshi:3 labels:v.1.1
 ALTER TABLE Employees
 ADD COLUMN mobile VARCHAR(15) AFTER phone;
+--rollbak ALTER TABLE Employees DROP COLUMN mobile
 
 -- changeset nikeshi:4 labels:v.1.1.0
 ALTER TABLE Employees
 ADD COLUMN address VARCHAR(15) AFTER mobile;
+--rollback ALTER TABLE Employees DROP COLUMN address
