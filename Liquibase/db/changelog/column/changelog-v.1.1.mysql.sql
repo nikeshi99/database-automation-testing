00018a2543ce0e945308ef0e3c94a4978c27ddfb
Nikeshi Kumarasinghe <nikeshikumarasinghe@gmail.com>
2022-06-21 14:34:31 +0530

    create new change log folder

--- Liquibase/db/changelog/column/changelog-v.1.1.mysql.sql
+++ Liquibase/db/changelog/column/changelog-v.1.1.mysql.sql
@@ -2,3 +2,7 @@
 -- changeset nikeshi:3 labels:v.1.1
 ALTER TABLE Employees
 ADD COLUMN mobile VARCHAR(15) AFTER phone;
+
+-- changeset nikeshi:4 labels:v.1.1.0
+ALTER TABLE Employees
+ADD COLUMN address VARCHAR(15) AFTER mobile;
