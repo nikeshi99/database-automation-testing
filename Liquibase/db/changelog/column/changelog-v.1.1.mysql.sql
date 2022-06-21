7d138e8f6e3733c09341b8ddbcf9b5b2cd051416
Nikeshi Kumarasinghe <nikeshikumarasinghe@gmail.com>
2022-06-21 18:58:10 +0530

    change to master.xml

--- Liquibase/db/changelog/column/changelog-v.1.1.mysql.sql
+++ Liquibase/db/changelog/column/changelog-v.1.1.mysql.sql
@@ -1,9 +1,11 @@
 -- liquibase formatted sql
--- changeset nikeshi:3 labels:v.1.1
+-- changeset nikeshi:3 
 ALTER TABLE Employees
 ADD COLUMN mobile VARCHAR(15) AFTER phone;
 
+--rollback drop column mobile;
 
--- changeset nikeshi:4 labels:v.1.1.0
+-- changeset nikeshi:4 
 ALTER TABLE Employees
 ADD COLUMN address VARCHAR(15) AFTER mobile;
+--rollback drop column address;
