9567af74bc3d615957015d916da56044074ebded
Nikeshi Kumarasinghe <nikeshikumarasinghe@gmail.com>
2022-06-22 17:06:12 +0530

    change changelog file

--- Liquibase/db/changelog/column/changelog-v.1.1.mysql.sql
+++ Liquibase/db/changelog/column/changelog-v.1.1.mysql.sql
@@ -14,13 +14,3 @@ ADD COLUMN address VARCHAR(15) AFTER mobile;
 ALTER TABLE clients
 ADD COLUMN mobile VARCHAR(15) AFTER phone;
 --rollback ALTER TABLE clients drop column mobile;
-
--- changeset nikeshi:6
-ALTER TABLE users
-ADD COLUMN mobile VARCHAR(15) AFTER phone;
---rollback ALTER TABLE users drop column mobile;
-
--- changeset nikeshi:7
-ALTER TABLE clients
-ADD COLUMN address VARCHAR(15) AFTER mobile;
---rollback ALTER TABLE clients drop column address;
