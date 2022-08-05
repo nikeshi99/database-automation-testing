54ae7d8ed4a4f4a61edfcf97635115215e1c1940
Nikeshi Kumarasinghe <nikeshikumarasinghe@gmail.com>
2022-08-05 21:44:31 +0530

    wdrw

--- nik/changelog/changelog-v.1.0.mysql.sql
+++ nik/changelog/changelog-v.1.0.mysql.sql
@@ -1,6 +1,6 @@
 -- liquibase formatted sql
 
--- changeset nikeshi:1655808533536-1 runOnChange:true
+-- changeset nikeshi:1 runOnChange:true
 CREATE TABLE Employees (employee_id INT AUTO_INCREMENT NOT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, phone VARCHAR(50) NOT NULL, hire_date date NOT NULL, manager_id INT NULL, job_title VARCHAR(255) NOT NULL, CONSTRAINT PK_EMPLOYEES PRIMARY KEY (employee_id));
 
 --rollback drop table Employees;
